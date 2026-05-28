248ad352c12d89f1e550b7f8cd05dae503272e16
Max Battcher <me@worldmaker.net>
2026-05-28 19:43:58 -0400

    Add an optional theme switch button

--- lcars-shell.js
+++ lcars-shell.js
@@ -11,6 +11,31 @@ function initializeShell() {
       co.scrollLeft += ev.deltaY
     }
   })
+  const themeswitch = document.getElementById("themeswitch")
+  if (themeswitch) {
+    const isLocalDark = localStorage.getItem('theme') === 'dark'
+    const isPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+    if (!isLocalDark && isPrefersDark) {
+      document.documentElement.classList.add('theme-light')
+    } else if (isLocalDark) {
+      document.documentElement.classList.add('theme-dark')
+    }
+    const isDark = isLocalDark || isPrefersDark || document.documentElement.classList.contains('theme-dark')
+    themeswitch.textContent = isDark ? '🌞' : '🌚'
+    themeswitch.addEventListener("click", (e) => {
+      const isDark = isPrefersDark && !document.documentElement.classList.contains('theme-light') || document.documentElement.classList.contains('theme-dark')
+      if (isDark) {
+        localStorage.setItem('theme', 'light')
+        document.documentElement.classList.remove('theme-dark')
+        document.documentElement.classList.add('theme-light')
+      } else {
+        localStorage.setItem('theme', 'dark')
+        document.documentElement.classList.remove('theme-light')
+        document.documentElement.classList.add('theme-dark')
+      }
+      themeswitch.textContent = !isDark ? '🌞' : '🌚'
+    })
+  }
   co.classList.add('columnar')
   const scrolldir = document.getElementById("scrolldir")
   scrolldir.classList.add('vis')
