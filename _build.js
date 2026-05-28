5064f83fb6424d8f6a10dddd4e1ecd363ec37aed
Max Battcher <me@worldmaker.net>
2026-05-28 15:18:44 -0400

    Clean up build script

---
+++ _build.js
@@ -0,0 +1,25 @@
+import { build } from 'esbuild'
+import sassPlugin from 'esbuild-plugin-sass'
+
+
+await build({
+    entryPoints: ['lcars-moderne.sass'],
+    bundle: true,
+    sourcemap: true,
+    minify: true,
+    outfile: 'dist/lcars-moderne.css',
+    plugins: [sassPlugin()],
+    loader: {
+        ".png": "dataurl",
+        ".woff": "copy",
+        ".woff2": "copy"
+    }
+})
+
+await build({
+    entryPoints: ['lcars-shell.js'],
+    bundle: true,
+    sourcemap: true,
+    minify: true,
+    outfile: 'dist/index.js'
+})
